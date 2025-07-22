ac6f6e3e88be8a58d3b60e944d414537b5b57410
kevinluo <luokev1@gmail.com>
2025-07-22 00:43:32 -0400

    gradient background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useState } from 'react';
 import StudyGroupGrid from './components/StudyGroupGrid';
 import { useStudyGroupData } from './components/UseStudyGroupData';
-import { StudyGroup } from './api/sheet';
+import LiquidSwirlBackground from './components/LiquidSwirlBackground';
+import GradientBackground from './components/GradientBackground';
 
 const App = () => {
   const studyGroups = useStudyGroupData();
 
   return (
     <div className="min-h-screen overflow-hidden">
+      <GradientBackground />
       <div className="relative z-10 min-h-screen flex flex-col items-center justify-center p-4">
         <StudyGroupGrid groups={studyGroups} />
       </div>
