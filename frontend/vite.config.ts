8cf9364eb17a57c696b0ba7b56a155775c287cc2
kevinluo <luokev1@gmail.com>
2025-07-25 19:18:57 -0400

    configuring vite env config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+  define: {
+    'process.env': env
+  },
 	plugins: [react()],
 	server: {
 		port: 3000,
 	},
 	build: {
 		outDir: "build",
-	},
+	},}
 })
