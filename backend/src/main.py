cf481ebcf9316c4b15c4162609e474b34cd4b80a
kevinluo <luokev1@gmail.com>
2025-08-15 17:07:13 -0400

    fly production launch config

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://localhost:3000", "https://yar8-attendance-app-ui.fly.dev"],
+    allow_origins=["http://localhost:8000", "http://localhost:3000", "https://yar8-attendance-app-ui.fly.dev", "https://yar8-attendance-app-ui-prod.fly.dev"],
     allow_methods=["*"],
     allow_headers=[],
 )
